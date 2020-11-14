b593e927fdfd987d143089e8122d481ea75d60f8
Patryk Buzowicz <patryk_buzowicz@hotmail.com>
2020-11-14 23:54:44 +0100

    add AJAX controller

---
+++ Wsei.ExchangeThings.Web/Controllers/ExchangesApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wsei.ExchangeThings.Web.Controllers
+{
+    [Route("[controller]")]
+    public class ExchangesApiController : ControllerBase
+    {
+        [HttpGet("/{id}")]
+        public string Get(string id)
+        {
+            return $"hello {id}";
+        }
+    }
+}
