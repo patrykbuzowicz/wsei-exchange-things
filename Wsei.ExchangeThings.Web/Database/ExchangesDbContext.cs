775558594354ed2c9ac56848e1f550a83c946011
Patryk Buzowicz <patryk_buzowicz@hotmail.com>
2020-11-14 23:49:16 +0100

    use sqlite

---
+++ Wsei.ExchangeThings.Web/Database/ExchangesDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Wsei.ExchangeThings.Web.Entities;
+
+namespace Wsei.ExchangeThings.Web.Database
+{
+    public class ExchangesDbContext : DbContext
+    {
+        public ExchangesDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ItemEntity> Items { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // fluent configuration ...
+        }
+    }
+}
